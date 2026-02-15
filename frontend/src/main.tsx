d9264172425eb12a7a1d92e1fad5163a828fb3d5
hellfire2024 <hellfire67878@gmail.com>
2026-02-15 03:00:29 -0500

    Fix: wrap App with SiteSettingsProvider to resolve useSiteSettings context error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import { ProductProvider } from "../context/ProductContext";
 import { ReviewsProvider } from "../context/ReviewsContext";
 import { PagesProvider } from "../context/PagesContext";
+import { SiteSettingsProvider } from "../context/SiteSettingsContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ProductProvider>
       <ReviewsProvider>
         <PagesProvider>
-          <App />
+          <SiteSettingsProvider>
+            <App />
+          </SiteSettingsProvider>
         </PagesProvider>
       </ReviewsProvider>
     </ProductProvider>
