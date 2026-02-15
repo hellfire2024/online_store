ac50125cab44375973e28238b5d905269578b92c
hellfire2024 <hellfire67878@gmail.com>
2026-02-15 02:46:53 -0500

    Fix: correct ProductProvider import path in frontend/src/main.tsx for Docker build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ProductProvider } from "../../context/ProductContext";
+import { ProductProvider } from "../context/ProductContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
