cfe59a218646d0f4aebb9ae2f6a7dfb6cc6c6d0a
hellfire2024 <hellfire67878@gmail.com>
2026-02-15 13:36:08 -0500

    Fix: restore local App.tsx as entry and wrap with all providers for Docker build compatibility

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,15 +6,55 @@ import HomePage from "../pages/HomePage";
 import StorePage from "../pages/StorePage";
 import AdminPage from "../pages/admin/AdminPage";
 
+
+import { CustomerAuthProvider } from "../context/CustomerAuthContext";
+import { CartProvider } from "../context/CartContext";
+import { ToastProvider } from "../hooks/useToast";
+import { AdminProvider, useAdmin } from "../context/AdminContext";
+import { UnsavedChangesProvider } from "../context/UnsavedChangesContext";
+import { ProductProvider } from "../context/ProductContext";
+import { GalleryProvider } from "../context/GalleryContext";
+import { StaffProvider } from "../context/StaffContext";
+import { ReviewsProvider } from "../context/ReviewsContext";
+import { ServicesProvider } from "../context/ServicesContext";
+import { PagesProvider } from "../context/PagesContext";
+import { SiteSettingsProvider } from "../context/SiteSettingsContext";
+import SiteEffectHandler from "../components/SiteEffectHandler";
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/store" element={<StorePage />} />
-        <Route path="/admin/*" element={<AdminPage />} />
-      </Routes>
-    </Router>
+    <AdminProvider>
+      <CustomerAuthProvider>
+        <UnsavedChangesProvider>
+          <SiteSettingsProvider>
+            <SiteEffectHandler />
+            <ProductProvider>
+              <GalleryProvider>
+                <StaffProvider>
+                  <ReviewsProvider>
+                    <ServicesProvider>
+                      <ToastProvider>
+                        <PagesProvider>
+                          <CartProvider>
+                            <Router>
+                              <Routes>
+                                <Route path="/" element={<HomePage />} />
+                                <Route path="/store" element={<StorePage />} />
+                                <Route path="/admin/*" element={<AdminPage />} />
+                              </Routes>
+                            </Router>
+                          </CartProvider>
+                        </PagesProvider>
+                      </ToastProvider>
+                    </ServicesProvider>
+                  </ReviewsProvider>
+                </StaffProvider>
+              </GalleryProvider>
+            </ProductProvider>
+          </SiteSettingsProvider>
+        </UnsavedChangesProvider>
+      </CustomerAuthProvider>
+    </AdminProvider>
   );
 }
 
