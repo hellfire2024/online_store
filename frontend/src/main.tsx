5e745baa47a778f6aa543ef538f8dc992847e1bd
hellfire2024 <hellfire67878@gmail.com>
2026-02-15 02:55:10 -0500

    Fix: wrap App with PagesProvider to resolve usePages context error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ProductProvider } from "../context/ProductContext";
 import { ReviewsProvider } from "../context/ReviewsContext";
+import { PagesProvider } from "../context/PagesContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ProductProvider>
       <ReviewsProvider>
-        <App />
+        <PagesProvider>
+          <App />
+        </PagesProvider>
       </ReviewsProvider>
     </ProductProvider>
   </React.StrictMode>,
