f0ea729216e432a5ac33ce77e949e74f423e2bdc
hellfire2024 <hellfire67878@gmail.com>
2026-02-15 13:06:03 -0500

    Fix: use monorepo App.tsx as entry in frontend/src/main.tsx for full provider tree

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,31 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { ProductProvider } from "../context/ProductContext";
-import { ReviewsProvider } from "../context/ReviewsContext";
-import { PagesProvider } from "../context/PagesContext";
-import { SiteSettingsProvider } from "../context/SiteSettingsContext";
-import { ToastProvider } from "../hooks/useToast";
-import { ServicesProvider } from "../context/ServicesContext";
-import { GalleryProvider } from "../context/GalleryContext";
+
+import App from "../../App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ProductProvider>
-      <ReviewsProvider>
-        <SiteSettingsProvider>
-          <ToastProvider>
-            <ServicesProvider>
-              <GalleryProvider>
-                <PagesProvider>
-                  <App />
-                </PagesProvider>
-              </GalleryProvider>
-            </ServicesProvider>
-          </ToastProvider>
-        </SiteSettingsProvider>
-      </ReviewsProvider>
-    </ProductProvider>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
