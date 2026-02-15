936b2757a4f66bc92946c6c37cf866b872822852
hellfire2024 <hellfire67878@gmail.com>
2026-02-15 02:51:35 -0500

    Fix: wrap App with ReviewsProvider to resolve useReviews context error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ProductProvider } from "../context/ProductContext";
+import { ReviewsProvider } from "../context/ReviewsContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ProductProvider>
-      <App />
+      <ReviewsProvider>
+        <App />
+      </ReviewsProvider>
     </ProductProvider>
   </React.StrictMode>,
 );
