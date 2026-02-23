52dc2c301228c16b7d98d9e61602cce0951a3eeb
hellfire2024 <hellfire67878@gmail.com>
2026-02-22 20:51:05 -0500

    chore(frontend): commit all frontend directory changes after move and import updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Import main pages
-import HomePage from "../../pages/HomePage";
-import StorePage from "../../pages/StorePage";
-import AdminPage from "../../pages/admin/AdminPage";
+import HomePage from "../pages/HomePage";
+import StorePage from "../pages/StorePage";
+import AdminPage from "../pages/admin/AdminPage";
 
-import { CustomerAuthProvider } from "../../context/CustomerAuthContext";
-import { CartProvider } from "../../context/CartContext";
-import { ToastProvider } from "../../hooks/useToast";
-import { AdminProvider, useAdmin } from "../../context/AdminContext";
-import { UnsavedChangesProvider } from "../../context/UnsavedChangesContext";
-import { ProductProvider } from "../../context/ProductContext";
-import { GalleryProvider } from "../../context/GalleryContext";
-import { StaffProvider } from "../../context/StaffContext";
-import { ReviewsProvider } from "../../context/ReviewsContext";
-import { ServicesProvider } from "../../context/ServicesContext";
-import { PagesProvider } from "../../context/PagesContext";
-import { SiteSettingsProvider } from "../../context/SiteSettingsContext";
-import SiteEffectHandler from "../../components/SiteEffectHandler";
+import { CustomerAuthProvider } from "../context/CustomerAuthContext";
+import { CartProvider } from "../context/CartContext";
+import { ToastProvider } from "../hooks/useToast";
+import { AdminProvider, useAdmin } from "../context/AdminContext";
+import { UnsavedChangesProvider } from "../context/UnsavedChangesContext";
+import { ProductProvider } from "../context/ProductContext";
+import { GalleryProvider } from "../context/GalleryContext";
+import { StaffProvider } from "../context/StaffContext";
+import { ReviewsProvider } from "../context/ReviewsContext";
+import { ServicesProvider } from "../context/ServicesContext";
+import { PagesProvider } from "../context/PagesContext";
+import { SiteSettingsProvider } from "../context/SiteSettingsContext";
+import SiteEffectHandler from "../components/SiteEffectHandler";
 
 function App() {
   return (
