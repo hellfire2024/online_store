fb90f60a8e458ad727f86acd96d477496605e3f9
hellfire2024 <hellfire67878@gmail.com>
2026-02-26 06:46:19 -0500

    Fix duplicate default exports in OrderManagement and CustomerManagement

--- frontend/pages/admin/CustomerManagement.tsx
+++ frontend/pages/admin/CustomerManagement.tsx
@@ -1,2 +1,6 @@
+import React from "react";
+const CustomerManagement: React.FC = () => {
+	return <div>Customer Management works!</div>;
+};
 export default CustomerManagement;
 // ...existing code...
--- frontend/pages/admin/OrderManagement.tsx
+++ frontend/pages/admin/OrderManagement.tsx
@@ -1,2 +1,6 @@
+import React from "react";
+const OrderManagement: React.FC = () => {
+	return <div>Order Management works!</div>;
+};
 export default OrderManagement;
 // ...existing code...
