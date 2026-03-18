35d23933f8fb693e87a5d0f708ff8f8131c2f924
hellfire2024 <hellfire67878@gmail.com>
2026-03-17 23:26:09 -0400

    Fix admin header overlap on resize

--- frontend/pages/admin/AdminPage.tsx
+++ frontend/pages/admin/AdminPage.tsx
@@ -73,7 +73,7 @@ const AdminPage: React.FC = () => {
             />
           </svg>
         </button>
-        <div className="w-full max-w-7xl mx-auto p-4 pt-20 sm:p-6 lg:p-8">
+        <div className="w-full max-w-7xl mx-auto p-4 pt-20 pl-20 sm:p-6 sm:pt-24 sm:pl-24 lg:p-8 lg:pt-8 lg:pl-8">
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route
